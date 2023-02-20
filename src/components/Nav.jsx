ac5846f0af4467809701179ee6593c927e3411c7
himanshu <hmxnsu.blvck@gmail.com>
2023-02-21 00:29:57 +0530

    nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CloseIcon from "@mui/icons-material/Close";
 import { motion } from "framer-motion";
+import { RandomReveal } from "react-random-reveal";
 
-export const Nav = () => {
+const Nav = () => {
+  const Navigate = useNavigate();
   // mobile nav
   const [navOpen, setNavOpen] = useState(false);
   const [navHeight, setNavHeight] = useState(0);
@@ -20,13 +22,12 @@ export const Nav = () => {
   // console.log(currentroute);
 
   const color = () => {
-    if (currentroute === "/about") {
-      return "text-[black]";
-    } else {
+    if (currentroute === "/") {
       return "text-[white]";
+    } else {
+      return "text-[black]";
     }
   };
-
 
   return (
     <motion.div
@@ -38,18 +39,26 @@ export const Nav = () => {
         ease: "easeInOut",
         times: [0, 1],
       }}
-      className=" Nav overflow-hidden flex flex-row w-full py-12 max-sm:py-4 max-sm:px-8 px-16 z-[10] text-white items-center justify-between absolute "
+      className=" Nav overflow-hidden flex flex-row w-full py-12 max-sm:py-4 max-sm:px-8 px-24 z-[90] text-white items-center justify-between absolute "
     >
-      <div className={`logo font-bold ${color()} `}>Name it.</div>
-      <div className={`navlink font-light max-md:hidden flex basis-2 flex-row text-[#989898] items-center justify-center `} >
-        <Link className=" duration-200 hover:text-[white] " to="/">
+      <Link to="/" className={`Link cursor-pointer logo font-bold ${color()} `}>
+        CINEREC
+        <span className="text-[red]  ">.</span>
+      </Link>
+      <div
+        className={`navlink font-light max-md:hidden flex basis-2 flex-row text-[#696969] items-center justify-center `}
+      >
+        <Link className=" duration-200 hover:text-[white] invert " to="/">
           Home
         </Link>
-        <Link className="mx-4 duration-200 hover:text-[white] ">Contact</Link>
-        <Link className=" duration-200 hover:text-[white] " to="/about">
+        <Link className="mx-4 duration-200 hover:text-[#d2d2d2] invert ">
+          Contact
+        </Link>
+        <Link className=" duration-200 hover:text-[white] invert " to="/about">
           About
         </Link>
       </div>
+      {/* mobile nav */}
       <div
         className="hamburger md:hidden flex flex-col justify-between w-8 cursor-pointer "
         onClick={toggleNav}
@@ -61,7 +70,7 @@ export const Nav = () => {
 
       {/* mobile nav */}
       <div
-        className={`mobile-nav flex flex-col justify-center items-center w-full h-[100vh] bg-[#000000b4] backdrop-blur-sm fixed top-0 left-0 z-[10] duration-200 ${
+        className={`mobile-nav flex flex-col justify-center items-center w-full h-[100vh] bg-[#000000b4] backdrop-blur-[5px] fixed top-0 left-0 z-[10] duration-200 ${
           navOpen ? "translate-x-0 blur-0 " : "translate-x-[120%] blur-[10px]"
         }`}
         ref={navRef}
@@ -75,7 +84,7 @@ export const Nav = () => {
             About
           </Link>
           <div
-            className="hamburger flex absolute right-8 top-8 flex-col justify-between w-8 cursor-pointer "
+            className="hamburger flex absolute right-12 max-sm:right-8 max-sm:top-8 top-10 flex-col justify-between w-8 cursor-pointer "
             onClick={toggleNav}
           >
             <CloseIcon />
@@ -85,3 +94,5 @@ export const Nav = () => {
     </motion.div>
   );
 };
+
+export default Nav;
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { Nav } from '../../components/Nav'
+import React from "react";
+import { RandomReveal } from "react-random-reveal";
+import Cursor from "../../components/Cursor";
+import Nav from "../../components/Nav";
+import img from "../assets/img3.jpg";
 
 const About = () => {
   return (
-    <div>
-        <Nav />
-    </div>
-  )
-}
+    <>
+      <style>
+        {`
+        .whole {
+          // background-image: url(${img});
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          background-blend-mode: screen;
+          // background-color: rgba(200, 200, 200, 1);
+          height: 100vh;
+          position: relative;
+          z-index: -1;
+        }
+        `}
+      </style>
+      <Cursor />
 
-export default About
+      <Nav />
+      <div className="textabout text-[20vw] font-black z-[-9] animate-scalingabout text-[#cad4de] absolute top-[-20%] max-lg:top-[-10%] max-md:top-[-8%] max-sm:top-[-5%]  ">
+        <RandomReveal isPlaying={true} duration={0.5} characters="ABOUT" />
+      </div>
+      <div className="whole backdrop-blur-[10px] max-md:backdrop-blur-sm ">
+        <div
+          style={{ height: "calc(100vh - 100px)" }}
+          className=" 
+          top-[100px] w-full h-full absolute backdrop-filter backdrop-blur-smd flex items-center
+        "
+        >\
+        
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
